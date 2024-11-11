2529972d53b6707c439c6cf4d88eb53aaf23b175
Kenzo Cogswell <kcogswell@usf.edu>
2024-11-10 22:02:40 -0500

    setting up html

--- Project/asset.js
+++ Project/asset.js
@@ -5,7 +5,7 @@ export const assets = [
     { id: 777, name: 'Nvidia', type: 'stock', price: 145, quantity: 30 },
     { id: 2009, name: 'U.S Treasury Bond', type: 'bond', price: 200, quantity: 45 },
     { id: 33647, name: 'West Meadows', type: 'real-estate', price: 3000, quantity: 2 },
-    { id: 33647, name: 'AMC', type: 'stock', price: 4, quantity: 975 },
+    { id: 31, name: 'AMC', type: 'stock', price: 4, quantity: 975 },
     { id: 1998, name: 'Charizard Card', type: 'collectible', price: 220, quantity: 13 }
 ]
 
---
+++ Project/main.js
@@ -0,0 +1,6 @@
+// Creating the Main Application: 
+
+// importing required data 
+import { calculatePortfolioValue, getPortfolioAllocation} from './portfolio.js';
+import { Transaction } from './transaction.js';
+
